4f2bc9ce1aeaa0d80fe3be23fdc78e8d34331f35
vcaruncho <vcaruncho@vcaruncho-HP.aytoonda.local>
2017-11-06 11:45:31 +0100

    Subida inicial de archivos

---
+++ EjemplosJava/src/cuadrado/Cuadrado.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadrado;
+
+/**
+ *
+ * @author vcaruncho
+ */
+public class Cuadrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=5;
+        int result;
+        result = num * num;
+        System.out.println("Cuadrado de " + num + ":" + result);
+    }
+    
+}
