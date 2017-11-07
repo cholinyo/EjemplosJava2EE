e48e532a8f8e16eba7b74505b2b6036819903946
vcaruncho <vcaruncho@vcaruncho-HP.aytoonda.local>
2017-11-07 08:57:36 +0100

    Movidos al paquete cuadrado

---
+++ EjemplosJava2EE/src/cuadrado/RecorridoParcial.java
@@ -0,0 +1,24 @@
+package cuadrado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vcaruncho
+ */
+public class RecorridoParcial {
+    public static void main(String[] args) {
+        int[] datos = {60 , 20 ,-5 ,8 ,9};
+        int n = 0;
+        while (n<datos.length && datos[n]>=0) {
+            System.out.println("dato" + datos[n]);
+            n ++;
+            }
+        System.out.println("numeros recuperados: " + n);
+    }
+    
+}
